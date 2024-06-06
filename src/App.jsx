a4179767f4e7d63a73a315f6e169c51dbff310ea
Shreyash Jain <shreyashjain41@gmail.com>
2024-06-06 23:50:17 +0530

    add : responsive storage and ram

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import HP_Header from './Components/HP_Header/HP_Header';
 import HP_Footer from './Components/HP_Footer/HP_Footer';
 import { Outlet } from 'react-router-dom';
 
-import OtherTwo from './Othertwo'
+// import OtherTwo from './Othertwo'
 // import HP_TopRatedProducts from './Components/HP_TopRated/HP_TopRated';
 
 function App(){
   return(
     <>
       <HP_Header/>
-        <Outlet/>
+      <Outlet/>
       <HP_Footer/>
     </>
   )
--- src/Components/HP_Product/HP_Product.jsx
+++ src/Components/HP_Product/HP_Product.jsx
@@ -1,68 +1,166 @@
-import  { useState } from 'react';
-import { FaStar, FaTags } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaStar, FaTags, FaShoppingCart, FaCreditCard } from 'react-icons/fa';
 import { MdCurrencyRupee } from 'react-icons/md';
-import { FiShare2 } from 'react-icons/fi';
 import ReactImageMagnify from 'react-image-magnify';
 
+// White 
 import iphone13 from '../../assets/images/iphone13.jpg';
 import iphone13One from '../../assets/images/iphone13_thumbnail_1.jpg';
 import iphone13Two from '../../assets/images/iphone13thumbnail_2.jpg';
-import iphoneThree from '../../assets/images/iphone13thumbnail_3.jpg';
-import iphoneFour from '../../assets/images/iphone13thumbnail_4.jpg';
+import iphone13Three from '../../assets/images/iphone13thumbnail_3.jpg';
+import iphone13Four from '../../assets/images/iphone13thumbnail_4.jpg';
 
-import './HP_Product.css';
+//Blue
+import blueIphoneOne from '../../assets/images/iPhone_Variants/blue/blueiphoneOne.jpg'
+import blueIphoneTwo from '../../assets/images/iPhone_Variants/blue/blueiphoneTwo.jpg'
+import blueIphoneThree from '../../assets/images/iPhone_Variants/blue/blueiphoneThree.jpg'
+import blueIphoneFour from '../../assets/images/iPhone_Variants/blue/blueiphoneFour.jpg'
+import blueIphoneFive from '../../assets/images/iPhone_Variants/blue/blueiphoneFive.jpg'
+
+//Green
+import greenIphoneOne from '../../assets/images/iPhone_Variants/Green/GreeniphoneOne.jpg'
+import greenIphoneTwo from '../../assets/images/iPhone_Variants/Green/GreeniphoneTwo.jpg'
+import greenIphoneThree from '../../assets/images/iPhone_Variants/Green/GreeniphoneThree.jpg'
+import greenIphoneFour from '../../assets/images/iPhone_Variants/Green/GreeniphoneFour.jpg'
+
+
+//Pink
+import pinkIphoneOne from '../../assets/images/iPhone_Variants/pink/pinkiphoneOne.jpg'
+import pinkIphoneTwo from '../../assets/images/iPhone_Variants/pink/pinkiphoneTwo.jpg'
+import pinkIphoneThree from '../../assets/images/iPhone_Variants/pink/pinkiphoneThree.jpg'
+import pinkIphoneFour from '../../assets/images/iPhone_Variants/pink/pinkiphoneFour.jpg'
 
 const rating = 5;
 const total_rating = 1;
 const total_reviews = 2;
 
-const op = 60000;
-const sp = 54000;
-
 const offer = [
-  'Get 200Rs Off Get 200Rs Off Get 200Rs Off Get 200Rs Off ',
+  'Get 200Rs Off',
   'Get 300Rs Off',
   'Get 400Rs Off',
   'Get 500Rs Off',
   'Get 600Rs Off',
 ];
 
-const imgArr = [iphone13, iphone13One, iphone13Two, iphoneThree, iphoneFour];
+const variants = [
+  {
+    color: 'White',
+    rams: [
+      { ram: '128GB', ogPrice: 65000, selprice: 54000, images: [iphone13, iphone13One, iphone13Two, iphone13Three, iphone13Four]},
+      { ram: '256GB', ogPrice: 70000, selprice: 58000, images: [iphone13, iphone13One, iphone13Two, iphone13Three, iphone13Four]},
+      { ram: '512GB', ogPrice: 75000, selprice: 62000, images: [iphone13, iphone13One, iphone13Two, iphone13Three, iphone13Four]},
+    ],
+  },
+  {
+    color: 'Blue',
+    rams: [
+      { ram: '128GB', ogPrice: 64000, selprice: 55000, images: [blueIphoneOne, blueIphoneTwo, blueIphoneThree, blueIphoneFour, blueIphoneFive ]},
+      { ram: '256GB', ogPrice: 72000, selprice: 58000, images: [blueIphoneOne, blueIphoneTwo, blueIphoneThree, blueIphoneFour, blueIphoneFive ]},
+      { ram: '512GB', ogPrice: 75000, selprice: 66000, images: [blueIphoneOne, blueIphoneTwo, blueIphoneThree, blueIphoneFour, blueIphoneFive ]},
+    ],
+  },
+  {
+    color: 'Green',
+    rams: [
+      { ram: '128GB', ogPrice: 65000, selprice: 54000, images: [greenIphoneOne, greenIphoneTwo, greenIphoneThree, greenIphoneFour] },
+      { ram: '256GB', ogPrice: 70000, selprice: 58000, images: [greenIphoneOne, greenIphoneTwo, greenIphoneThree, greenIphoneFour] },
+      { ram: '512GB', ogPrice: 75000, selprice: 62000, images: [greenIphoneOne, greenIphoneTwo, greenIphoneThree, greenIphoneFour] },
+    ],
+  },
+  {
+    color: 'Pink',
+    rams: [
+      { ram: '128GB', ogPrice: 65000, selprice: 54000, images: [pinkIphoneOne, pinkIphoneTwo, pinkIphoneThree, pinkIphoneFour] },
+      { ram: '256GB', ogPrice: 70000, selprice: 58000, images: [pinkIphoneOne, pinkIphoneTwo, pinkIphoneThree, pinkIphoneFour] },
+      { ram: '512GB', ogPrice: 75000, selprice: 62000, images: [pinkIphoneOne, pinkIphoneTwo, pinkIphoneThree, pinkIphoneFour]},
+    ],
+  },
+  
+];
+
+import './HP_Product.css';
 
 export default function HP_Product() {
-  const [gallery, setGallery] = useState(imgArr[0]);
+  const [selectedColorVariant, setSelectedColorVariant] = useState(variants[0]);
+  const [selectedRamVariant, setSelectedRamVariant] = useState(selectedColorVariant.rams[0]);
+  const [gallery, setGallery] = useState(selectedRamVariant.images[0]);
   const [selectedIndex, setSelectedIndex] = useState(0);
 
+  const handleColorChange = (colorVariant) => {
+    setSelectedColorVariant(colorVariant);
+    setSelectedRamVariant(colorVariant.rams[0]);
+    setGallery(colorVariant.rams[0].images[0]);
+    setSelectedIndex(0);
+  };
+
+  const handleRamChange = (ramVariant) => {
+    setSelectedRamVariant(ramVariant);
+    setGallery(ramVariant.images[0]);
+    setSelectedIndex(0);
+  };
+
   const handleOnClick = (index) => {
-    setGallery(imgArr[index]);
+    setGallery(selectedRamVariant.images[index]);
     setSelectedIndex(index);
+  };
+
+  const displayPrice = (ramVariant) => {
+    if (ramVariant.selprice) {
+      return (
+        <div className="d-flex flex-row align-items-end">
+          <h4>
+            <MdCurrencyRupee className="mb-1" />
+            {ramVariant.selprice}
+          </h4>
+          <div className="text-decoration-line-through text-secondary mx-2">
+            <MdCurrencyRupee className="mb-2" />
+            {ramVariant.ogPrice}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <h4>
+          <MdCurrencyRupee className="mb-1" />
+          {ramVariant.ogPrice}
+        </h4>
+      );
+    }
+  };
+
+  const handleAddToCart = () => {
+    // Logic to add item to cart
+    console.log('Item added to cart');
+  };
+
+  const handleBuyNow = () => {
+    // Logic to proceed with buying the item
+    console.log('Proceed to buy the item');
   };
 
   return (
     <>
       <div className="mt-5"></div>
-      {/* In this a page is divided into two parts */}
-      <div className="d-flex flex flex-lg-row flex-md-row flex-sm-row flex-column overflow-scroll justify-content-between mx-5 p-2 shadow shadow-2 ">
-        {/* Section I */}
+      <div className="d-flex flex-lg-row flex-column overflow-scroll justify-content-between mx-5 p-2 shadow shadow-2">
         <div className="col-lg-6">
-          <div className="d-flex flex mx-2 m-auto ">
-            {imgArr.length > 0 ? (
-              <div className="d-flex flex-column col-lg-2 col-md-2 col-sm-3 col-2">
-                {imgArr.map((data, index) => (
+          <div className="d-flex flex m-auto">
+            {selectedRamVariant.images.length > 0 && (
+              <div className="d-flex flex-column col-lg-1 col-md-1 col-sm-1 col-2">
+                {selectedRamVariant.images.map((data, index) => (
                   <div
                     key={index}
                     onMouseEnter={() => handleOnClick(index)}
                     className={selectedIndex === index ? 'border border-2 border-primary-subtle' : ''}
                   >
-                    <img src={data} alt={`Thumbnail ${index + 1}`} className='col-lg-12 col-md-10 col-sm-12 col-12'/>
+                    <img src={data} alt={`Thumbnail ${index + 1}`} className="img-fluid my-1" />
                   </div>
                 ))}
               </div>
-            ) : null}
-            <div className="border border-2 align-content-center mx-2">
+            )}
+            <div className="border border-2 col-lg-9 col-md-6 col-sm-6 align-content-center mx-2">
               <ReactImageMagnify
                 smallImage={{
-                  isFluidWidth: true, // Change to true for fluid width
+                  isFluidWidth: true,
                   src: gallery,
                 }}
                 largeImage={{
@@ -72,42 +170,39 @@ export default function HP_Product() {
                 }}
                 isHintEnabled={true}
                 shouldHideHintAfterFirstActivation={false}
+                isEnlargedImagePortalEnabledForTouch={true}
               />
             </div>
           </div>
         </div>
 
-        {/* Section II */}
-        <div className="d-flex flex flex-column col-lg-6 ok ">
+        <div className="d-flex flex-column col-lg-6 mt-3 mt-lg-0">
           <div className="d-flex flex">
-            <h3>Apple iPhone 13 (Starlight, 128 GB)</h3>
-            <FiShare2 />
+            <h4>Apple iPhone 13 ({selectedColorVariant.color}, {selectedRamVariant.ram})</h4>
           </div>
 
-          <div className="d-flex flex  justify-content-between">
+          <div className="d-flex flex justify-content-between">
             <p>Apple</p>
             <p className="mx-3">Apple Inc.</p>
           </div>
 
-          <div className="d-flex flex flex-row ">
-            <span className={`d-flex flex`}>
+          <div className="d-flex flex flex-row">
+            <span className="d-flex flex">
               {total_rating > 0 ? (
                 <>
-                  {rating >= 0 ? (
+                  {rating >= 0 && (
                     <>
                       <div
                         style={{ height: '25px' }}
-                        className={`p-1 d-flex flex ${
-                          rating >= 3.5 ? 'bg-success' : rating <= 1 ? 'bg-danger' : 'bg-warning'
-                        } rounded rounded-2`}
+                        className={`p-1 d-flex flex ${rating >= 3.5 ? 'bg-success' : rating <= 1 ? 'bg-danger' : 'bg-warning'} rounded rounded-2`}
                       >
-                        <h6 className="text-light ">{rating}</h6>
+                        <h6 className="text-light">{rating}</h6>
                         <FaStar className="text-light mx-2" />
                       </div>
                       <p className="text-secondary mx-2">Ratings {total_rating}</p>
                       <p className="text-secondary mx-2">Reviews {total_reviews}</p>
                     </>
-                  ) : null}
+                  )}
                 </>
               ) : (
                 <p>No Ratings yet...</p>
@@ -116,41 +211,59 @@ export default function HP_Product() {
           </div>
 
           <div className="d-flex flex flex-row">
-            {op === sp ? (
-              <h4>
-                <MdCurrencyRupee className="mb-1" />
-                {sp}
-              </h4>
-            ) : (
-              <>
-                <h4>
-                  <MdCurrencyRupee className="mb-1" />
-                  {sp}
-                </h4>
-                <div className="d-flex flex align-items-end mb-1 text-decoration-line-through text-secondary">
-                  <MdCurrencyRupee className="mb-2" />
-                  <h6>{op}</h6>
-                </div>
-              </>
-            )}
+            {displayPrice(selectedRamVariant)}
           </div>
 
           <div className="d-flex flex flex-column">
-            {offer.length !== 0 ? (
+            {offer.length !== 0 && (
               <>
                 <h5>Available Offers</h5>
                 <div className="mx-3">
                   {offer.map((data, index) => (
-                    <div key={index} className='d-flex flex'>
-                    <h5>
-                      <FaTags className="text-success mx-2"></FaTags></h5>
-                      <div className="">{data}</div>
+                    <div key={index} className="d-flex flex">
+                      <h5>
+                        <FaTags className="text-success mx-2" />
+                      </h5>
+                      <div>{data}</div>
                       <br />
                     </div>
                   ))}
                 </div>
               </>
-            ) : null}
+            )}
+          </div>
+
+          <div className="d-flex flex-lg-row flex-column mt-3">
+            <h6 className='text-secondary align-content-center'>Color</h6>
+            <div className="d-flex flex">
+              {variants.map((variant, index) => (
+                <img
+                  key={index}
+                  src={variant.rams[0].images[0]}
+                  alt={variant.color}
+                  onClick={() => handleColorChange(variant)}
+                  className={`col-lg-2 col-md-1 col-sm-1 col-2 mx-1 img-fluid   rounded rounded-circle shadow shadow-2  ${variant.color === selectedColorVariant.color ? 'border border-3 border-info' : 'border border-2  border-info-subtle'}`}
+                />
+              ))}
+            </div>
+          
+            <h6 className='text-secondary align-content-center'>Ram</h6>
+            <div className="d-flex flex">
+              {selectedColorVariant.rams.map((ramVariant, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleRamChange(ramVariant, console.log(ramVariant))}
+                  className={`btn mx-1 mb-1 align-self-center ${ramVariant.ram === selectedRamVariant.ram ? 'active' : ''}`}
+                >
+                  {ramVariant.ram}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          <div className="d-flex flex-lg-row flex-column mt-3">
+          <button className="btn btn-outline-primary m-1 p-3 flex-fill shadow shadow-1" onClick={handleAddToCart}><FaShoppingCart className="me-2" /> Add to Cart</button>
+            <button className="btn btn-primary m-1 p-3 flex-fill shadow shadow-1" onClick={handleBuyNow}><FaCreditCard className="me-2" /> Buy Now</button>
           </div>
         </div>
       </div>
