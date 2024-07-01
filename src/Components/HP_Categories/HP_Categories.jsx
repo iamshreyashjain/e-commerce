1cf60b60a166b2dc4bb5f69e3941df8d184e6014
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-02 02:26:17 +0530

    responsive-2 commit

--- src/Components/HP_Categories/HP_Categories.jsx
+++ src/Components/HP_Categories/HP_Categories.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import "./HP_Categories.css";
 
@@ -9,24 +8,24 @@ export default function HP_Categories() {
       caption: 'Grocery'
     },
     {
-      image: 'https://as1.ftcdn.net/v2/jpg/01/88/21/46/1000_F_188214656_C6ZaKov6XHjtpSslDXg8Nym8zE4zzNa2.jpg',
-      caption: 'Grocery'
+      image: 'https://img.freepik.com/free-vector/household-appliances-realistic-composition_1284-65307.jpg?t=st=1719866554~exp=1719870154~hmac=b177e506c98cf1fd953b1065e376d646567d4152cf18159c6a13e2dce3e68065&w=1800',
+      caption: 'Electronics'
     },
     {
-      image: 'https://as1.ftcdn.net/v2/jpg/01/88/21/46/1000_F_188214656_C6ZaKov6XHjtpSslDXg8Nym8zE4zzNa2.jpg',
-      caption: 'Grocery'
+      image: 'https://img.freepik.com/free-photo/beautiful-men-fashion-wooden-background_74190-2613.jpg?t=st=1719866794~exp=1719870394~hmac=ca73b5555d80104d3dec71e091b7ef0d4998deb56fa81b01778bfc58cb161dda&w=1480',
+      caption: 'Accessories'
     },
     {
-      image: 'https://as1.ftcdn.net/v2/jpg/01/88/21/46/1000_F_188214656_C6ZaKov6XHjtpSslDXg8Nym8zE4zzNa2.jpg',
-      caption: 'Grocery'
+      image: 'https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?t=st=1719866889~exp=1719870489~hmac=86b97473c922781e6e928599844950c5af6d891e5d0779b34b5699f8c51b1e86&w=1480',
+      caption: 'Sports'
     },
     {
-      image: 'https://as1.ftcdn.net/v2/jpg/01/88/21/46/1000_F_188214656_C6ZaKov6XHjtpSslDXg8Nym8zE4zzNa2.jpg',
-      caption: 'Grocery'
+      image: 'https://img.freepik.com/free-photo/high-angle-group-childrens_23-2148445714.jpg?t=st=1719867015~exp=1719870615~hmac=c429d473f34801ceb7d8b1033e6349d478b780133406dd61b26f4f078ebd177c&w=1480',
+      caption: 'Kids'
     },
     {
-      image: 'https://as1.ftcdn.net/v2/jpg/01/88/21/46/1000_F_188214656_C6ZaKov6XHjtpSslDXg8Nym8zE4zzNa2.jpg',
-      caption: 'Grocery'
+      image: 'https://img.freepik.com/free-photo/empty-modern-room-with-furniture_23-2149178335.jpg?t=st=1719867060~exp=1719870660~hmac=fb2bd41a11d2530d6f5b2d7d002e14e4a1850d2f5eab1532025802b7e2e7ab34&w=1480',
+      caption: 'Lifestyle'
     },
     // Add more categories as needed
   ];
@@ -34,12 +33,12 @@ export default function HP_Categories() {
   return (
     <>
       <div className="my-5"></div>
-      <div className="d-flex flex-row justify-content-between my-2">
+      <div className="d-flex flex-row justify-content-between my-2 bannerMargin">
         <div><h4>Categories</h4></div>
       </div>
 
       <div className="my-1"></div>
-      <div className="d-flex flex-row justify-content-lg-evenly justify-content-md-around justify-content-sm-around  justify-content-evenly   flex-wrap p-2 shadow shadow-1">
+      <div className="d-flex flex-row justify-content-lg-evenly justify-content-md-around justify-content-sm-around  justify-content-evenly flex-wrap p-2 shadow shadow-1 bannerMargin">
         {categories.map((category, index) => (
           <div className="d-flex flex-column" key={index}>
             <Link to="" className="text-decoration-none text-dark">
--- src/Components/HP_Slider/HP_Slider.jsx
+++ src/Components/HP_Slider/HP_Slider.jsx
@@ -1,14 +1,14 @@
-import BannerOne from "../../assets/images/BannerOne.jpg"
-import BannerTwo from "../../assets/images/BannerTwo.jpg"
-import BannerThree from "../../assets/images/BannerThree.jpg" 
-import BannerFour from "../../assets/images/BannerFour.jpg"
-import BannerFive from "../../assets/images/BannerFive.jpg"
-import BannerSix from "../../assets/images/BannerSix.jpg"
-
 import "./HP_Slider.css"
 
 export default function HP_Slider(){
-    const banners = [BannerOne, BannerTwo, BannerThree , BannerFour, BannerFive, BannerSix];
+    const banners = [
+'https://img.freepik.com/free-vector/vibrant-color-black-friday-festive-sale-banner-with-50-off_1017-40612.jpg?t=st=1719866309~exp=1719869909~hmac=aac26fa03b912876a302d8e92a1fd031fcc3d28b84b9aaea6e63dae5412b3991&w=1800',
+'https://img.freepik.com/premium-vector/happy-diwali-hindu-festival-banner-diya-with-creative-design_30996-1313.jpg?w=1800',
+'https://img.freepik.com/free-vector/bright-geometrical-black-friday-sale-banner-design-vector_1055-9127.jpg?t=st=1719866384~exp=1719869984~hmac=0c444362e81db4a2880c78d092598655d78f6411a3e9a29ab25006d8f9224395&w=1800',
+'https://img.freepik.com/free-vector/happy-diwali-sale-offer-banner-with-artistic-diya-vector_1017-40482.jpg?t=st=1719866424~exp=1719870024~hmac=5d0649f3f5f11bfaa4a388d92d3dee943f85e97dd09c5aae017ba91d86d8c3e5&w=1800',
+'https://img.freepik.com/free-vector/raksha-bandhan-festival-sale-discount-banner_1017-19712.jpg?t=st=1719866520~exp=1719870120~hmac=ecf38b8f32785cf2ee728bbdc5fa4851176c49eef6b87fa9117b2b0b32d52fc0&w=1800'
+
+    ];
     return(
       <div className="m-5">
         <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
@@ -16,7 +16,7 @@ export default function HP_Slider(){
             {banners.map((data, index) => (
               <div key={index}>
               <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
-                <img src={data} className="d-block w-100" />
+                <img src={data} className="d-block w-100" height={250}/>
               </div>
               </div>
             ))}
