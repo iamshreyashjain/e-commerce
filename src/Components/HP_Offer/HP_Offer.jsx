7caa3b68dd7d25b4fa23fa53efe3577a176cc39a
Shreyash Jain <shreyashjain41@gmail.com>
2024-07-23 00:31:15 +0530

    eComm  commit

--- src/Components/HP_Offer/HP_Offer.jsx
+++ src/Components/HP_Offer/HP_Offer.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
-import './HP_Offer.css';
 
 export default function HP_Offer() {
   const [offers, setOffers] = useState([]);
@@ -23,15 +22,15 @@ export default function HP_Offer() {
     <>
     <div className="my-5"></div>
     <div className="d-flex flex-row justify-content-between mx-2">
-      <div><h4>Categories</h4></div>
+      <div><h4>Offers</h4></div>
     </div>
 
     <div className="my-1"></div>
-    <div className="d-flex flex-row justify-content-between flex-wrap p-2 shadow shadow-1 mx-2">
+    <div className="d-flex flex-row justify-content-between flex-wrap p-2 shadow shadow-1">
       {offers.map((category, index) => (
         <div className="d-flex flex-column mb-3 col-lg-2 col-md-2 col-sm-4 col-12 " key={index}>
           <Link to="" className="text-decoration-none text-dark">
-            <div className="catImg-container">
+          <div className="catImg-container">
               <img 
                 src={category.image} 
                 alt={category.caption} 
